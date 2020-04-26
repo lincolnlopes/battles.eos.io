c7b75ed0d552ab0cb3ae1cb2b48d597f8a5afeb5
Lincoln Lopes <lincolnlopes@msn.com>
2020-04-26 02:51:45 -0300

    Armazenando Estado e Login

---
+++ frontend/src/components/Game/Game.js
@@ -0,0 +1,10 @@
+// React core
+import React, { Component } from "react";
+
+class Game extends Component {
+  render() {
+    return <section className="Game">Welcome!</section>;
+  }
+}
+
+export default Game;
